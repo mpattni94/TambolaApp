efc7ab6027abc63b06381be49c40db0a91c7cc29
mpattni <mpattni94@gmail.com>
2020-07-05 22:52:53 +0530

    Create GenericConstants.java

---
+++ src/main/java/com/tambola/app/constants/GenericConstants.java
@@ -0,0 +1,7 @@
+package com.tambola.app.constants;
+
+public interface GenericConstants{
+
+  char[] charArray = "0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm".toCharArray();
+  
+}
