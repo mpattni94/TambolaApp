390332df6f5619ee8bf704fd541cabfef21d9459
mpattni <mpattni94@gmail.com>
2020-07-06 00:19:55 +0530

    Create RandomNumberUtils.java

---
+++ src/main/java/com/tambola/app/utils/random/RandomNumberUtils.java
@@ -0,0 +1,24 @@
+package com.tambola.app.utils.random;
+
+import com.tambola.app.GenericConstants;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class RandomNumberUtils{
+  Random random = new Random();
+  
+  public String generateRandomString(int stringLength){
+    if(stringLength == 0){
+      stringLength = 8;
+    }
+    
+    StringBuilder sb = new StringBuilder();
+    
+    for(int i=0; i<stringLength; i++){
+       sb.append(GenericConstants.charArray{random.nextInt(GenericConstants.charArray.length)]);
+    }
+    return sb.toString();
+  }
+}
