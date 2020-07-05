eb01f9da99a0b65cfb1a2a4d3bf5dca99d97cd9b
mpattni <mpattni94@gmail.com>
2020-07-05 22:56:37 +0530

    Create ResultStrings.java

---
+++ src/main/java/com/tambola/app/constants/ResultStrings.java
@@ -0,0 +1,12 @@
+package com.tambola.app.constants;
+
+public interface ResultStrings{
+
+  String INVALID_USER_ID = "lease ensure a valid game id is provided along with a valid and unique user name";
+  String INVALID_TICKET_ID = "Invalid ticket ID. Please enter correct one";
+  String INVALID_GAME_ID = "Invalid game ID. Please enter a correct one";
+  String NO_NUMBERS_DRAWN_YET = "No numbers drawn yet";
+  String ALL_NUMBERS_DRAWN = "All numbers drawn for this game";
+  String SERVER_ERROR = "Server error. Please try again in sometime";
+
+}
