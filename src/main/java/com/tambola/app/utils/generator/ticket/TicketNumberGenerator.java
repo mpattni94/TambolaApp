29bbf304277e52fcd54b6a76a4a479f55a69947a
mpattni <mpattni94@gmail.com>
2020-07-06 00:13:12 +0530

    Create TicketNumberGenerator.java

---
+++ src/main/java/com/tambola/app/utils/generator/ticket/TicketNumberGenerator.java
@@ -0,0 +1,36 @@
+package com.tambola.app.utils.generator.ticket;
+
+import tambola.app.beans.Game;
+import tambola.app.constants.TicketConstants;
+import com.tambola.app.utils.redis.RepositoryUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+import java.util.Set;
+
+@Component
+public class TicketNumberGenerator{
+  
+  @Autowired
+  RepositoryUtils repositoryUtils
+  
+  Random random = new Random();
+  
+  public int generateRandomNumberForGameAndStore(String gameId){
+    int number;
+    Game game = repositoryUtils.retrieveGameObjectFromRedis(gameId);
+    Set<String> numbersAlreadyDone = game.getNumbersDoneSoFar();
+    if(numbersAlreadyDone.size() < TicketConstants.HIGHEST_VALUE_ALLOWED){
+      do{
+        number = random.nextInt(TicketConstants.HIGHEST_VALUE_ALLOWED);
+      }while(numbersAlreadyDone.contains(number + "") || number == 0);
+      numbersAlreadyDone.add(number + "");
+      game.setNumbersDoneSoFar(numbersAlreadyDone);
+      repositoryUtils.saveToRedis(game);
+    }else{
+      number = -1;
+    }
+    return number;
+  }
+}
