90328af791786437caa1909f2cf34428abb21b2e
mpattni <mpattni94@gmail.com>
2020-07-06 01:44:54 +0530

    Update ResultStrings.java

--- src/main/java/com/tambola/app/constants/ResultStrings.java
+++ src/main/java/com/tambola/app/constants/ResultStrings.java
@@ -2,7 +2,7 @@ package com.tambola.app.constants;
 
 public interface ResultStrings{
 
-  String INVALID_USER_ID = "lease ensure a valid game id is provided along with a valid and unique user name";
+  String INVALID_USER_ID = "Please ensure a valid game id is provided along with a valid and unique user name";
   String INVALID_TICKET_ID = "Invalid ticket ID. Please enter correct one";
   String INVALID_GAME_ID = "Invalid game ID. Please enter a correct one";
   String NO_NUMBERS_DRAWN_YET = "No numbers drawn yet";
