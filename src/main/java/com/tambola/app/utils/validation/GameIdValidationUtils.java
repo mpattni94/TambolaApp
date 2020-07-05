ac3ce48850587787da2d19f4b4aa410169ccf957
mpattni <mpattni94@gmail.com>
2020-07-06 00:46:39 +0530

    Create GameIdValidationUtils.java

---
+++ src/main/java/com/tambola/app/utils/validation/GameIdValidationUtils.java
@@ -0,0 +1,36 @@
+package com.tambola.app.utils.validation;
+
+import com.tambola.app.beans.Game;
+import com.tambola.app.constants.GameConstants;
+import com.tambola.app.utils.redis.RepositoryUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.NoSuchElementException;
+
+public class GameIdValidationUtils{
+  @Autowired
+  RepositoryUtils repositoryUtils;
+  
+  public boolean isGameValid(final String gameId){
+    return ((isLengthValid(gfameId) && gameIdExists(gameId));
+  }
+  
+  public boolean isLengthValid(String gameId){
+    boolean result = false;
+    if(gameId.length() == GameConstants.GAME_ID_LENGTH){
+      result = true;
+    }
+    return result;
+  }
+  
+  public boolean gameIdExists(String gameId){
+    boolean result = true;
+    try{
+      Game game = repositoryUtils.retrieveGameObjectFromRedis(gameId);
+    }catch(NoSuchElementException e){
+        result = false;
+    }
+    return result;
+  }
+}
